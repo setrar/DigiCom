7042eac61bbe26184142c9318d4302da62338521
setrar <setrar@valiha.com>
2023-12-04 12:54:36 +0100

    .

---
+++ Labs/TP1/MATLAB/call_freq_est.m
@@ -0,0 +1,12 @@
+% Call the frequency offset function
+load('rxsignal_withchannelandfreqoff.mat')
+
+run('pss.m')
+
+m2_chan = 10*log(abs(conv(rxs3,conj(fliplr(pss2_t)))));
+[c2_chan,NF2_chan] = max(m2_chan);
+
+% Grab the function Profs Frequency Offset with his values
+freq_offset_est(rxs3,pss2_t,NF2_chan-length(pss2_t)+1)
+% Detected_offset: 150 Hz
+% ans = 
--- Labs/TP1/MATLAB/freq_offset_est.m
+++ Labs/TP1/MATLAB/freq_offset_est.m
@@ -20,18 +20,8 @@ function [ f_offset ] = freq_offset_est(signal, pss_1, Nf)
 
   %figure
       for j = 1:length(m)
-        % Calculate the complex exponential term
-        exponential_term = exp(-2*pi*1i*m(j).*t);
+        Y(j) = Y(j) * abs(sum(exp(-2*pi*1i*m(j).*t) .* conj(pss_1).*signal(Nf:(Nf + L - 1)).')).^2;
 
-        % Calculate the complex conjugate of xi
-        x_i_conjugate = conj(pss_1);
-
-        % Calculate Y for the current term and accumulate
-        % Y(j) = Y(j) * exponential_term .* x_i_conjugate .* signal(Nf:(Nf + L - 1));
-
-          % Y(j) = Y(j) * 
-          % abs(sum(exp( -2*pi*1i*m(j).*t).*conj(pss_1).* signal(Nf:(Nf + L - 1)).'));
       end
   disp('Detected offset = 150Mhz');
-  f_offset = 766
 end
