ce89a91637df369fba2e14627ae6c8e89da16d4e
setrar <setrar@valiha.com>
2023-12-08 22:31:04 +0100

    .

--- Labs/TP1/matlab/freq_offset_est.m
+++ Labs/TP1/matlab/freq_offset_est.m
@@ -1,40 +1,31 @@
 % Filename: freq_offset_est.m
-function [ f_offset ] = freq_offset_est(signal, pss_1, Nf)
+function [ f_offset ] = freq_offset_est(signal, pss, Nf, m, Fs)
   %%Frequency offset estimator
 
-  DELTA_F = 10;
-
-  Fs = 61.44e6;
-  f_min = -7500;
-  f_max = 7500;
-
-  m = f_min:DELTA_F:f_max;
-  Y = zeros(1,length(m));
-  L = length(pss_1);
+  Y = zeros(1,length(m));  
+  
+  L = length(pss);
   t = 0:(1/Fs):((L-1)/Fs);
 
   signal_part = signal(Nf:(Nf + L - 1));
-  trans = signal_part.'
-  transConj = conj(pss_1).* trans;
+  trans = signal_part.';
+  transConj = conj(pss).* trans;
 
   for j = 1:length(m)
     exp_comp = exp(-2*pi*1i*m(j).*t);
-    fprintf('exp_comp %d \n',length(exp_comp));
     signal_offset = sum(exp_comp.*transConj);
-    value = abs(signal_offset).^2
+    value = abs(signal_offset).^2;
+    fprintf('adding %d at %d\n',value,j);
     Y(j) = Y(j) + value;
   end
   
-  [A_fo, fo] = max(Y)
-  % disp('Detected offset = 150Mhz',real(m2_chan));
+  [A_fo, fo] = max(Y);
+  fprintf('m %d, A_fo %d, fo %d \n',length(m),10 * log10(A_fo),fo);
 
-  fprintf('m %d, A_fo %d, fo %d \n',length(m),A_fo,fo);
+  % disp('Detected offset = 150Mhz',real(m2_chan));
 
   figure;
   subtitle('Frequency offset');
-
-  plot(1:length(m),Y,".")
-
-  length(Y)
+  plot(1:length(m),10 * log10(abs(Y)),".")
 
 end
