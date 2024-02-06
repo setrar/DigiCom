e35c161a6ffda46545385c717e38ded130ecd312
setrar <setrar@valiha.com>
2024-02-06 15:51:31 +0100

    .

--- Labs/data/matlab/freq_offset_est.m
+++ Labs/data/matlab/freq_offset_est.m
@@ -15,7 +15,7 @@ function [ f_offset ] = freq_offset_est(signal, pss_t, Nf)
   L = length(pss_t);
   t = 0:(1/Fs):((L-1)/Fs);
 
-  for j = 1:length(m)
+  for j = 1:length(m)                   
     Y(j) = Y(j) + abs(sum(exp(-2*pi*1i*m(j).*t).* conj(pss_t).* signal(Nf:(Nf + L - 1)).')).^2;
   end
   
@@ -24,10 +24,13 @@ function [ f_offset ] = freq_offset_est(signal, pss_t, Nf)
 
   % disp('Detected offset = 150Mhz',real(m2_chan));
 
-  m(fo)
+  % disp('Detected_offset = %dMhz',m(fo));
+  fprintf('Detected_offset = %d Hz\n', m(fo));
 
   figure;
   subtitle('Frequency offset');
   plot(m,10 * log10(abs(Y)),".")
+
+  % f_offset = Y
 
 end
